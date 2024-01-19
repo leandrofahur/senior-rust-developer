966b93d1cc5764125bcb6a7c087405a5af83fd80
Leandro Miranda Fahur Machado <fahurleandro@gmail.com>
2024-01-19 13:17:05 -0800

    mod structure and importing from separate files

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,19 +1,20 @@
-fn add_five(num:u32) -> u32 {
-    num + 5
-}
+mod my_functions;
+
+// If we want to use all the functions in my_functions module
+// use crate::my_functions::*;
 
 // Everthng in Rust is immutable by default
 // To make a variable mutable, we must use the mut keyword
 
 fn main() {
     // immutable variable
-    let num = 10;
-    println!("{} + 5 = {}", num, add_five(num));
+    let num: u32 = 10;
+    println!("{} + 5 = {}", num, my_functions::add_five(num));
 
     // mutable variable
-    let mut x = 10;
+    let mut x: i32 = 10;
     println!("x = {}", x);
+    my_functions::display_address(&x);
     x = 20;
-    println!("x = {}", x);
-    
+    println!("x = {}", x);    
 }
---
+++ playground/src/my_functions.rs
@@ -0,0 +1,7 @@
+pub fn add_five(num:u32) -> u32 {
+  num + 5
+}
+
+pub fn display_address(num_addr: &i32) {
+  println!("Address of x is {:p}", num_addr);
+}
